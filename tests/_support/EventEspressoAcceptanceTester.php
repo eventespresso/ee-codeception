d0b8165fb3e91c001d93e2620b093cac418db16c
Darren Ethier <darren@roughsmootheng.in>
2017-04-30 12:22:17 -0400

    add phpdocs

--- tests/_support/EventEspressoAcceptanceTester.php
+++ tests/_support/EventEspressoAcceptanceTester.php
@@ -1,7 +1,23 @@
 <?php
 
+/**
+ * EventEspressoAcceptanceTester
+ * This actor contains methods used for EventEspresso Core tests.
+ *
+ * @package EventEspresso Acceptance Tests
+ * @author  Darren Ethier
+ * @since   1.0.0
+ */
 class EventEspressoAcceptanceTester extends AcceptanceTester
 {
+    /**
+     * EventEspressoAcceptanceTester constructor.
+     * By default, implementing this actor will ensure that the EventEspresso core plugin is active.
+     * You can control that by instantiating it with the `$activate` flag set to false.
+     *
+     * @param \Codeception\Scenario $scenario
+     * @param bool                  $activate
+     */
     public function __construct(\Codeception\Scenario $scenario, $activate = true)
     {
         parent::__construct($scenario);
@@ -11,6 +27,10 @@ class EventEspressoAcceptanceTester extends AcceptanceTester
     }
 
 
+    /**
+     * Ensures the Event Espresso core plugin is activated.  However this will not cause any test fails.
+     * @throws Exception
+     */
     public function ensureCoreActivated()
     {
         $I = $this;
@@ -24,11 +44,16 @@ class EventEspressoAcceptanceTester extends AcceptanceTester
             $I->logOut();
         } catch (Exception $e) {
             //do nothing except logout because its already active.
+            echo "Event Espresso core plugin is already active.";
             $I->logOut();
         }
     }
 
 
+    /**
+     * This ensures the Event Espresso core plugin is deactivated.  However this will not cause any test fails.
+     * @throws Exception
+     */
     public function ensureCoreDeactivated()
     {
         $I = $this;
@@ -42,11 +67,16 @@ class EventEspressoAcceptanceTester extends AcceptanceTester
             $I->logOut();
         } catch (Exception $e) {
             //do nothing except logout because its already deactivated.
+            echo "Event Espresso core plugin is already deactivated.";
             $I->logOut();
         }
     }
 
 
+    /**
+     * Logs out from the WordPress instance.
+     * Before calling this, the actor should be logged in.
+     */
     public function logOut()
     {
         $I = $this;
