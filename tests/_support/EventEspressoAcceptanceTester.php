5f1b3a2a3e12baf0e6223d358e587676c5c35ac1
Darren Ethier <darren@roughsmootheng.in>
2017-04-30 12:07:52 -0400

    tweak EventEspresso Actor so that we are only ensuring Activation of core #4
    When running successive tests that use this actor, we just need to make sure the plugin is active but we don’t need to try to activate it if its already active.

--- tests/_support/EventEspressoAcceptanceTester.php
+++ tests/_support/EventEspressoAcceptanceTester.php
@@ -6,20 +6,52 @@ class EventEspressoAcceptanceTester extends AcceptanceTester
     {
         parent::__construct($scenario);
         if ($activate) {
-            $this->activateCore();
+            $this->ensureCoreActivated();
         }
     }
 
 
-    public function activateCore()
+    public function ensureCoreActivated()
     {
         $I = $this;
-        $I->wantTo('Activate Event Espresso Core');
         $I->loginAsAdmin();
         $I->amOnPluginsPage();
         $I->waitForText('Plugins');
-        $I->seePluginDeactivated('event-espresso');
-        $I->activatePlugin('event-espresso');
-        $I->see('Welcome to Event Espresso');
+        try {
+            $I->seePluginDeactivated('event-espresso');
+            $I->activatePlugin('event-espresso');
+            $I->see('Welcome to Event Espresso');
+            $I->logOut();
+        } catch (Exception $e) {
+            //do nothing except logout because its already active.
+            $I->logOut();
+        }
+    }
+
+
+    public function ensureCoreDeactivated()
+    {
+        $I = $this;
+        $I->loginAsAdmin();
+        $I->amOnPluginsPage();
+        $I->waitForText('Plugins');
+        try {
+            $I->seePluginActivated('event-espresso');
+            $I->deactivatePlugin('event-espresso');
+            $I->see('Plugin deactivated');
+            $I->logOut();
+        } catch (Exception $e) {
+            //do nothing except logout because its already deactivated.
+            $I->logOut();
+        }
+    }
+
+
+    public function logOut()
+    {
+        $I = $this;
+        $I->moveMouseOver('#wp-admin-bar-my-account');
+        $I->click("li#wp-admin-bar-logout > a.ab-item");
+        $I->see("You are now logged out.");
     }
 }
