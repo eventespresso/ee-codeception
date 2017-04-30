246fc72e4c7c34b2a7efb6a6b01ae637bf509906
Darren Ethier <darren@roughsmootheng.in>
2017-04-30 12:09:54 -0400

    rework run-locally script
    - allow setting start_from_scratch variable.
    - export variables (so setup-tests.sh pick them up)
    - start and stop webservices (php webserver and phantomjs) before and after running tests.

--- run-locally.sh
+++ run-locally.sh
@@ -1,27 +1,43 @@
 #!/usr/bin/env bash
-while getopts "hb:t:a:" opt; do
-    case $opt in
+usage() {
+    echo "\n\n\tusage: sh run-locally.sh [ -h This usage instructions ] [ -s start from scratch ] [ -b ee-branch ] [ -t ee-core-tag ] [ -a ee-addon-package-slug ]"
+    echo "\texample: To run tests with ee core branch 'FET-12345-some-work'"
+    echo "\tsh run-locally.sh -b FET-12345-some-work\n\n"
+    exit 2
+}
+
+while getopts "hsb:t:a:" opt; do
+    case ${opt} in
         h)
-            echo "\n\n\tusage: sh run-locally.sh [-h This usage instructions] [-b ee-branch] [-t ee-core-tag] [-a ee-addon-package-slug]"
-            echo "\texample: To run tests with ee core branch 'FET-12345-some-work'"
-            echo "\tsh run-locally.sh -b FET-12345-some-work\n\n"
-            exit 1
+            usage
             ;;
         b)
-            EE_BRANCH=$OPTARG
+            export EE_BRANCH=$OPTARG
             ;;
         t)
-            EE_TAG=$OPTARG
+            export EE_TAG=$OPTARG
             ;;
         a)
-            ADDON_PACKAGE=$OPTARG
+            export ADDON_PACKAGE=$OPTARG
+            ;;
+        s)
+            export START_FROM_SCRATCH=true
             ;;
         :)
             echo "Option -$OPTARG requires an argument." >&2
             exit 1
             ;;
+        \?)
+            usage
+            ;;
     esac
 done
+
+##make sure webservers are started
+./server-services start
+
 ./setup-tests.sh
 echo "Running Acceptance Tests with Codeception..."
 php ./vendor/bin/wpcept run
+## stop webservers
+./server-services stop
