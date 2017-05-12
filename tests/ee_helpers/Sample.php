f53d1e65575b788574d8e75d609a1e98b482951e
Darren Ethier <darren@roughsmootheng.in>
2017-05-12 11:33:25 -0400

    Sample Helper (does not get included in builds)

---
+++ tests/ee_helpers/Sample.php
@@ -0,0 +1,12 @@
+<?php
+namespace EventEspresso\Codeception\Helpers;
+
+/**
+ * Trait Sample
+ * EE Helpers are traits that should be included in the Acceptance actors.
+ *
+ * @package EventEspresso\Codeception\Helpers
+ */
+trait Sample {
+
+}
