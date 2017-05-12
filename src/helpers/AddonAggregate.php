51f1c9d5de76d76f3e2bb29fe3344c1fd876090e
Darren Ethier <darren@roughsmootheng.in>
2017-05-12 11:32:59 -0400

    templates for the aggregate traits.

---
+++ src/helpers/AddonAggregate.php
@@ -0,0 +1,10 @@
+<?php
+namespace EventEspresso\Codeception\helpers;
+
+trait AddonAggregate
+{
+    /**
+     * This is auto-generated during the build process. Do not add anything to this class
+     * because it will get removed during the build process.
+     */
+}
---
+++ src/helpers/CoreAggregate.php
@@ -0,0 +1,12 @@
+<?php
+namespace EventEspresso\Codeception\helpers;
+
+trait CoreAggregate
+{
+    /**
+     * This is auto-generated during the build process. Do not add anything to this class
+     * because it will get removed during the build process.
+     */
+    use Test;
+
+}
