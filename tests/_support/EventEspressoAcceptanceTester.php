f91591f1471f1f4f9de6b8c29b22f6e82db862a2
Darren Ethier <darren@roughsmootheng.in>
2017-05-15 18:32:03 -0400

    tweak actor so that it allows for flexibility on plugin activation/deactivation methods

--- tests/_support/EventEspressoAcceptanceTester.php
+++ tests/_support/EventEspressoAcceptanceTester.php
@@ -42,13 +42,14 @@ class EventEspressoAcceptanceTester extends AcceptanceTester
         $I->amOnPluginsPage();
         $I->waitForText('Plugins');
         try {
-            $I->seePluginDeactivated('event-espresso');
-            $I->activatePlugin('event-espresso');
-            $I->see('Welcome to Event Espresso');
+            $I->seePluginDeactivated('event-espresso', true);
+            $I->activatePlugin('event-espresso', true);
+            $I->waitForText('Welcome to Event Espresso');
         } catch (Exception $e) {
-            //do nothing except logout because its already active.
+            //do nothing except logout because its already deactivated.
             echo "\nEvent Espresso core plugin is already active.\n";
         }
+        //do nothing except logout because its already active.
         $I->logOut();
     }
 
@@ -64,8 +65,8 @@ class EventEspressoAcceptanceTester extends AcceptanceTester
         $I->amOnPluginsPage();
         $I->waitForText('Plugins');
         try {
-            $I->seePluginActivated('event-espresso');
-            $I->deactivatePlugin('event-espresso');
+            $I->seePluginActivated('event-espresso', true);
+            $I->deactivatePlugin('event-espresso', true);
             $I->see('Plugin deactivated');
         } catch (Exception $e) {
             //do nothing except logout because its already deactivated.
@@ -87,5 +88,116 @@ class EventEspressoAcceptanceTester extends AcceptanceTester
         $I->see('Log Out', '.ab-item');
         $I->click("li#wp-admin-bar-logout > a.ab-item");
         $I->see("You are now logged out.");
+    }
+
+
+    /**
+     * Overriding default WPBrowser::activatePlugin() method because it requires an exact match for plugin slug.
+     * I want to do a partial match (which will work with our version specific slugs)
+     *
+     * @param array|string $pluginSlug
+     * @param bool         $exact   Whether to do an exact match for the slug or partial match on the start of the slug.
+     */
+    public function activatePlugin($pluginSlug, $exact = false)
+    {
+        $plugins = (array)$pluginSlug;
+        foreach ($plugins as $plugin) {
+            $option = $exact
+                ? '//*[@data-slug="' . $plugin . '"]/th/input'
+                : "//tr[starts-with(@data-slug,'$plugin')]/th/input";
+            $this->scrollTo($option, 0, -40);
+            $this->checkOption($option);
+        }
+        $this->scrollTo('select[name="action"]', 0, -40);
+        $this->selectOption('action', 'activate-selected');
+        $this->click("#doaction");
+    }
+
+
+    /**
+     * Overriding default WPBrowser::deactivatePlugin() method because it requires an exact match for plugin slug.
+     * I want to do a partial match (which will work with our version specific slugs)
+     *
+     * @param array|string $pluginSlug
+     * @param bool         $exact   Whether to do an exact match for the slug or partial match on the start of the slug.
+     */
+    public function deactivatePlugin($pluginSlug, $exact = false) {
+        $plugins = (array) $pluginSlug;
+        foreach ($plugins as $plugin) {
+            $option = $exact
+                ? '//*[@data-slug="' . $plugin . '"]/th/input'
+                : "//tr[starts-with(@data-slug,'$plugin')]/th/input";
+            $this->scrollTo($option, 0, -40);
+            $this->checkOption($option);
+        }
+        $this->scrollTo('select[name="action"]', 0, -40);
+        $this->selectOption('action', 'deactivate-selected');
+        $this->click("#doaction");
+    }
+
+
+    /**
+     * Overriding default WPBrowser::seePluginActivated() method because it requires an exact match for plugin slug.
+     * I want to do a partial match (which will work with our version specific slugs)
+     *
+     * @param array|string $pluginSlug
+     * @param bool         $exact   Whether to do an exact match for the slug or partial match on the start of the slug.
+     */
+    public function seePluginActivated($pluginSlug, $exact = false)
+    {
+        $this->seePluginInstalled($pluginSlug, $exact);
+        if ($exact) {
+            $this->seeElement("table.plugins tr[data-slug='$pluginSlug'].active");
+        } else {
+            $this->seeElement("//tr[@class='active' and starts-with(@data-slug, '$pluginSlug')]");
+        }
+    }
+
+
+    /**
+     * Overriding default WPBrowser::seePluginDeactivated() method because it requires an exact match for plugin slug.
+     * I want to do a partial match (which will work with our version specific slugs)
+     *
+     * @param array|string $pluginSlug
+     * @param bool         $exact   Whether to do an exact match for the slug or partial match on the start of the slug.
+     */
+    public function seePluginDeactivated($pluginSlug, $exact = false)
+    {
+        $this->seePluginInstalled($pluginSlug, $exact);
+        if ($exact) {
+            $this->seeElement("table.plugins tr[data-slug='$pluginSlug'].inactive");
+        } else {
+            $this->seeElement("//tr[@class='inactive' and starts-with(@data-slug, '$pluginSlug')]");
+        }
+    }
+
+    /**
+     * Overriding default WPBrowser::seePluginInstalled() method because it requires an exact match for plugin slug.
+     * I want to do a partial match (which will work with our version specific slugs)
+     *
+     * @param array|string $pluginSlug
+     * @param bool         $exact   Whether to do an exact match for the slug or partial match on the start of the slug.
+     */
+    public function seePluginInstalled($pluginSlug, $exact = false)
+    {
+        if ($exact) {
+            $this->seeElement("table.plugins tr[data-slug='$pluginSlug']");
+        } else {
+            $this->seeElement("//tr[starts-with(@data-slug, '$pluginSlug')]");
+        }
+    }
+
+
+
+    /**
+     * Overriding default WPBrowser::dontSeePluginInstalled() method because it requires an exact match for plugin slug.
+     * I want to do a partial match (which will work with our version specific slugs)
+     *
+     * @param array|string $pluginSlug
+     * @param bool         $exact   Whether to do an exact match for the slug or partial match on the start of the slug.
+     */
+    public function dontSeePluginInstalled($pluginSlug, $exact = false)
+    {
+        $this->dontSeeElement("//tr[starts-with(@data-slug, '$pluginSlug')]");
     }
 }
