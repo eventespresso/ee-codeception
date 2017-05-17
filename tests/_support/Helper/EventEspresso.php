2b03130152401d4a5e782651052f94e80663ab64
Darren Ethier <darren@roughsmootheng.in>
2017-05-16 20:46:05 -0400

    add EventEspresso helper for acceptance tests [ci skip]
    This will contain any module related helper functions/methods that can be used for accessing things for tests.

---
+++ tests/_support/Helper/EventEspresso.php
@@ -0,0 +1,35 @@
+<?php
+namespace Helper;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+use Codeception\Module\WPWebDriver;
+use Facebook\WebDriver\Exception\ElementNotVisibleException;
+use Facebook\WebDriver\Remote\RemoteWebElement;
+
+class EventEspresso extends \Codeception\Module
+{
+    /**
+     * Use this to get the Url from a given link locater.
+     *
+     * @param $locater
+     * @return string
+     * @throws ElementNotVisibleException
+     * @throws \Codeception\Exception\ModuleException
+     */
+    public function observeLinkUrlAt($locater)
+    {
+        return $this->getModule('WPWebDriver')->grabAttributeFrom($locater, 'href');
+    }
+
+
+    /**
+     * Use this to retrieve value from an input for the given locater.
+     * @param $locater
+     */
+    public function observeValueFromInputAt($locater)
+    {
+        return $this->getModule('WPWebDriver')->grabValueFrom($locater);
+    }
+}
