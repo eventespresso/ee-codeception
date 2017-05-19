c93209e483b00de09c5046c09c15d8b8fa83c4d2
Darren Ethier <darren@roughsmootheng.in>
2017-05-19 08:18:25 -0400

    just triggering build

--- src/helpers/CoreAggregate.php
+++ src/helpers/CoreAggregate.php
@@ -7,5 +7,4 @@ trait CoreAggregate
      * This is auto-generated during the build process. Do not add anything to this class
      * because it will get removed during the build process.
      */
-
 }
