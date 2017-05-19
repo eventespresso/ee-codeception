945a5c87e36a2a7379b67365a717bf51de548124
Darren Ethier <darren@roughsmootheng.in>
2017-05-19 08:13:47 -0400

    remove old stuff [ci skip]

--- src/helpers/CoreAggregate.php
+++ src/helpers/CoreAggregate.php
@@ -7,6 +7,5 @@ trait CoreAggregate
      * This is auto-generated during the build process. Do not add anything to this class
      * because it will get removed during the build process.
      */
-    use Test;
 
 }
