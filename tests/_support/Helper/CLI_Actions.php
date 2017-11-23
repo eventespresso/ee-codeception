d5057100bb449259d2c196c97f44f66f1da15f20
Darren Ethier <darren@roughsmootheng.in>
2017-11-23 11:14:15 -0500

    modify existing actor action and add new one [ci-skip]

--- tests/_support/Helper/CLI_Actions.php
+++ tests/_support/Helper/CLI_Actions.php
@@ -38,13 +38,13 @@ class CLI_Actions extends CodeCeptionModule
 
 
     /**
-     * Use to verify if a the next run timestamp for the given cron event is what's expected.
+     * Use to verify if a the next run timestamp for the given cron event is as expected.
      *
      * @param string $hook
      * @param string $expected_next_run  expected in format YYYY-MM-DD 00:00:00
      * @throws ModuleException
      */
-    public function seeCronEventNextRunIs($hook, $expected_next_run)
+    public function seeCronEventNextRunIsSet($hook, $expected_next_run)
     {
         $command = 'event list --hook=' . $hook . ' --format=json --fields=hook,next_run_gmt';
         $output = array();
@@ -67,6 +67,42 @@ class CLI_Actions extends CodeCeptionModule
                         'The actual next_run schedule was %s.  Expected was %s.',
                         $event->next_run_gmt,
                         $expected_next_run
+                    )
+                );
+            }
+        }
+    }
+
+
+    /**
+     * Use to verify if the schedule for the given cron event is as expected.
+     *
+     * @param string $hook
+     * @param string $expected_schedule Something like 'daily', 'twicedaily', or any custom schedule identifier setup.
+     * @throws ModuleException
+     */
+    public function seeCronEventScheduleIsSet($hook, $expected_schedule) {
+        $command = 'event list --hook=' . $hook . ' --format=json --fields=hook,schedule';
+        $output = array();
+        $command_result = $this->getModule('WPCLI')->cli($command, $output);
+        if ($command_result) {
+            $events = json_decode($output[0]);
+            $event = $events[0];
+            //should have hook and should have expected_next_run.
+            if (empty($event)) {
+                $this->fail(
+                    sprintf(
+                        'The cron event hook named %s is not set.',
+                        $hook
+                    )
+                );
+            }
+            if ($event->schedule !== $expected_schedule) {
+                $this->fail(
+                    sprintf(
+                        'The actual schedule was %s.  Expected was %s.',
+                        $event->next_run_gmt,
+                        $expected_schedule
                     )
                 );
             }
