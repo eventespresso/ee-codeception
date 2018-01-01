6ff3c571a3c6934f2bf065262df3e30fbdb47348
Darren Ethier <darren@roughsmootheng.in>
2018-01-01 16:54:56 -0500

    woops forgot to bring over user changes into acceptance-tests-entry.sh

--- docker/scripts/acceptance-tests-entry.sh
+++ docker/scripts/acceptance-tests-entry.sh
@@ -5,11 +5,22 @@ declare -F log &>/dev/null && echo "" || source common.sh
 
 log "info" "Running on host: $(hostname)"
 
+## set defaults
+MY_GROUP='accuser'
+MY_USER='accuser'
+
+## setup user if necessary
 ## setup user if necessary
 if [ $(id accuser >/dev/null 2>&1) ]; then
-    log "info" "'accuser' user already exists no need to setup"
+    log "info" "'accuser' user already exists - deleting then resetting up with proper uids"
+    delgroup ${MY_USER} ${MY_GROUP}
+    deluser ${MY_USER}
+    delgroup ${MY_GROUP}
+    source ./setup-user.sh
 else
     source ./setup-user.sh
+    ### Change ownership of all the files in the user directory.
+    run "chown -R ${MY_USER}:${MY_GROUP} /home/accuser"
 fi
 
 ### setup wp-cli and composer if necessary
