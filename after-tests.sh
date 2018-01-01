03bc820f263319035736e2971d8654ca6b80b42f
Darren Ethier <darren@roughsmootheng.in>
2018-01-01 10:51:58 -0500

    fix paths for travis artifact upload

--- after-tests.sh
+++ after-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd ${PROJECT_ROOT}/tests/
+cd ./tests/
 
 artifacts upload --target-paths "artifacts/ee-codeception/${TRAVIS_BUILD_ID}/${TRAVIS_JOB_ID}" _output
 
-cd ${PROJECT_ROOT}
+cd ../
